e9fa37d3b59297e376dd74efc0fe74b0e2760bed
root <root@vamoss.com.br>
2020-02-28 18:49:18 -0500

    fix wsgi Sentry import

--- config/wsgi.py
+++ config/wsgi.py
@@ -25,7 +25,7 @@ app_path = os.path.abspath(os.path.join(
 sys.path.append(os.path.join(app_path, 'bootcamp'))
 
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+    from raven.middleware import Sentry
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
