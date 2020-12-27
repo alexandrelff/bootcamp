896f9026dcaba705cf8e0a997d1d71b836d70339
Vamoss <vamoss@vamoss.com.br>
2020-12-27 14:55:36 -0300

    export users csv

--- bootcamp/users/admin.py
+++ bootcamp/users/admin.py
@@ -2,8 +2,10 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.http import HttpResponse
 
 from bootcamp.users.models import User
+import csv, datetime
 
 
 class MyUserChangeForm(UserChangeForm):
@@ -30,6 +32,27 @@ class MyUserCreationForm(UserCreationForm):
 
         raise forms.ValidationError(self.error_messages["duplicate_username"])
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' 'filename{}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response, delimiter=";")
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+export_to_csv.short_description = 'Export to CSV'  #short description
 
 @admin.register(User)
 class MyUserAdmin(AuthUserAdmin):
@@ -38,3 +61,4 @@ class MyUserAdmin(AuthUserAdmin):
     fieldsets = (("User Profile", {"fields": ("name",)}),) + AuthUserAdmin.fieldsets
     list_display = ("username", "name", "is_superuser")
     search_fields = ["name"]
+    actions = [export_to_csv]
